56d0ab3c72acc553d990faaed1dacd36008c4912
brandonmontes-alt <gabriell.gomez@discoverystudent.org>
2025-11-04 11:30:33 -0500

    11-4

--- Assets/Scripts/player_conroller_script.cs
+++ Assets/Scripts/player_conroller_script.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player_conroller_script : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        rb = GetComponent<Rigidbody>();
 
-    // Update is called once per frame
-    void Update()
+    }
+    private void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY); 
+        rb.AddForce(movement);
+    } 
+    // Update is called once per frame
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movement= movementValue.Get<Vector2>();
+        movementX = movement.x;
+        movementY = movement.y;
     }
 }
