9e2a857a685b12a181c89e6808ef77ae8012c681
daniemigh-png <dani.emigh@discoverystudent.org>
2025-11-17 10:39:03 -0500

    11-17-25

--- Assets/Scripts/Player_Controller_Script.cs
+++ Assets/Scripts/Player_Controller_Script.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro; 
 
 public class player_conroller_script : MonoBehaviour
 {
@@ -7,16 +8,29 @@ public class player_conroller_script : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 0;
+public TextMeshProUGUI countText; 
+private int count; 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start( count = 0; )
     {
         rb = GetComponent<Rigidbody>();
 
     }
-    private void FixedUpdate()
+    
+    void OnTriggerEnter (Collider other)
+    {
+     if (other.gameObject.CompareTag("PickUp"))
+    {
+         other.gameObject.SetActive(false);
+    }
+    count = count +1; 
+    }
+private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY); 
-        rb.AddForce(movement);
+        rb.AddForce (movement);
     } 
     // Update is called once per frame
     void OnMove(InputValue movementValue)
@@ -24,5 +38,10 @@ public class player_conroller_script : MonoBehaviour
         Vector2 movement= movementValue.Get<Vector2>();
         movementX = movement.x;
         movementY = movement.y;
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count" + count.ToString();
     }
 }
